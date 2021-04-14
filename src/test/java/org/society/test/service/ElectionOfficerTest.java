738b604eae949b79a3a9b881a2baff307fa7766c
Govind1121 <k.govind4@gmail.com>
2021-04-14 19:26:42 +0530

    Update ElectionOfficerTest.java

--- src/test/java/org/society/test/service/ElectionOfficerTest.java
+++ src/test/java/org/society/test/service/ElectionOfficerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.society.dao.ElectionOfficerDao;
 import org.society.entities.ElectionOfficer;
 import org.society.repository.ElectionOfficerRepository;
-import org.society.service.ElectionOfficerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
